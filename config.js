efafeec6f8f23a0f6bead8f547293bbe6586c976
TuanAnhdeeptry <92804053+TuanAnhdeeptry@users.noreply.github.com>
2021-10-19 21:24:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    titleWeb: "CacbanyeucuaTA",
+    introTitle: 'Các bạn yêu của TA',
+    introDesc: `Bởi vì sự giận dỗi mà TA đẫ out nhóm, vì vậy nhấn Ok tao mới nói tiếp được`,
+    btnIntro: 'Ok',
+    title: 'Hãy bỏ qua cho TA nha 🥰',
+    desc: 'Ép buộc phải bỏ qua ',
+    btnYes: 'Sao mà hông bỏ được',
+    btnNo: 'Đéo',
+    question: 'Tao biết là sẽ bỏ qua cho tao mà, yêuuuu',
+    btnReply: 'Gửi cho TA đẹp trai',
+    reply: 'Tại vì mày đẹp trai quá nên bỏ qua đó',
+    mess: 'Tao biết mà 🥰. Yêu các bạn nhiều nhiều',
+    messDesc: 'Tối thứ 7 đi trà sữa cho nó căng cực đi',
+    btnAccept: 'Ok luôn',
+    messLink: 'https://www.messenger.com/t/100054663648552/'
 }
